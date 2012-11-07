b0b5e65f11ffab3eaf9340a7ea9008a75ab705c8
Ichi Dong <ichi1234567@gmail.com>
2012-11-07 10:35:44 +0800

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curiosity::Application.config.secret_token = 'f9aa677e4e8559607981f6d4df892aab6c6022d10a0d89efd02aceff8a75a36937fc7a4cbd87c608198b7893569dc2ff36ae8ab63ac3c5958e0361a3f0ad47ae'
